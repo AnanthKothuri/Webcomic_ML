3a19dbe337c775f90bba41700f168b0374a6b0b0
Ananth Kothuri <ananth.s.kothuri@gmail.com>
2024-02-20 13:13:25 -0600

    small updates

--- data/person_detection/yolo_detection.py
+++ data/person_detection/yolo_detection.py
@@ -12,83 +12,83 @@ classes = ['person']
 COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
 
 
-def get_output_layers(net):
+# def get_output_layers(net):
     
-    layer_names = net.getLayerNames()
-    try:
-        output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
-    except:
-        output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+#     layer_names = net.getLayerNames()
+#     try:
+#         output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+#     except:
+#         output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
-    return output_layers
+#     return output_layers
 
 
-def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+# def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
 
-    label = str(classes[class_id])
+#     label = str(classes[class_id])
 
-    color = COLORS[class_id]
+#     color = COLORS[class_id]
 
-    cv2.rectangle(img, (x,y), (x_plus_w,y_plus_h), color, 2)
+#     cv2.rectangle(img, (x,y), (x_plus_w,y_plus_h), color, 2)
 
-    cv2.putText(img, label, (x-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+#     cv2.putText(img, label, (x-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
     
-def detect_people(img_path):
-    image = cv2.imread(img_path)
+# def detect_people(img_path):
+#     image = cv2.imread(img_path)
 
-    Width = image.shape[1]
-    Height = image.shape[0]
-    scale = 0.00392
+#     Width = image.shape[1]
+#     Height = image.shape[0]
+#     scale = 0.00392
 
-    net = cv2.dnn.readNet(weights_path, cfg_path)
+#     net = cv2.dnn.readNet(weights_path, cfg_path)
 
-    blob = cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
+#     blob = cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
 
-    net.setInput(blob)
+#     net.setInput(blob)
 
-    outs = net.forward(get_output_layers(net))
+#     outs = net.forward(get_output_layers(net))
 
-    class_ids = []
-    confidences = []
-    boxes = []
-    conf_threshold = 0.5
-    nms_threshold = 0.4
-
-
-    for out in outs:
-        for detection in out:
-            scores = detection[5:]
-            class_id = np.argmax(scores)
-            confidence = scores[class_id]
-            if confidence > 0.5:
-                center_x = int(detection[0] * Width)
-                center_y = int(detection[1] * Height)
-                w = int(detection[2] * Width)
-                h = int(detection[3] * Height)
-                x = center_x - w / 2
-                y = center_y - h / 2
-                class_ids.append(class_id)
-                confidences.append(float(confidence))
-                boxes.append([x, y, w, h])
+#     class_ids = []
+#     confidences = []
+#     boxes = []
+#     conf_threshold = 0.5
+#     nms_threshold = 0.4
 
 
-    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+#     for out in outs:
+#         for detection in out:
+#             scores = detection[5:]
+#             class_id = np.argmax(scores)
+#             confidence = scores[class_id]
+#             if confidence > 0.5:
+#                 center_x = int(detection[0] * Width)
+#                 center_y = int(detection[1] * Height)
+#                 w = int(detection[2] * Width)
+#                 h = int(detection[3] * Height)
+#                 x = center_x - w / 2
+#                 y = center_y - h / 2
+#                 class_ids.append(class_id)
+#                 confidences.append(float(confidence))
+#                 boxes.append([x, y, w, h])
 
-    for i in indices:
-        try:
-            box = boxes[i]
-        except:
-            i = i[0]
-            box = boxes[i]
+
+#     indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+
+#     for i in indices:
+#         try:
+#             box = boxes[i]
+#         except:
+#             i = i[0]
+#             box = boxes[i]
         
-        x = box[0]
-        y = box[1]
-        w = box[2]
-        h = box[3]
-        draw_prediction(image, class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h))
+#         x = box[0]
+#         y = box[1]
+#         w = box[2]
+#         h = box[3]
+#         draw_prediction(image, class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h))
 
-    return image, box, confidences[i]
+#     return image, box, confidences[i]
 
 
 def detect_faces_helper(img_path):
